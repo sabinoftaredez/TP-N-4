883448d65f29671bce4891afc3aff005c45adf62
sabinoftaredez <sabinoftaredez@gmail.com>
2025-02-21 18:19:31 -0300

    Modificacion del txt y mjs

--- TPN4/proyect-root/Models/superhéroes.mjs
+++ TPN4/proyect-root/Models/superhéroes.mjs
@@ -1,15 +1,30 @@
 export default class superhéroes {
-    constructor (id, nombre, atributo, edad)
-        {this.id = id;
-        this.nombre = nombre;
-        this.atributo = atributo;
-        this.edad = edad;        
+    constructor (id, nombreSuperhéroe, nombreReal, nombreSociedad, edad, planetaOrigen, debilidad, poder, habilidadEspecial, aliado, enemigo)
+    {
+        this.id = id;
+        this.nombreSuperhéroe = nombreSuperhéroe;
+        this.nombreReal = nombreReal;
+        this.nombreSociedad = nombreSociedad;
+        this.edad = edad;
+        this.planetaOrigen = planetaOrigen;
+        this.debilidad = debilidad;
+        this.poder = poder;
+        this.habilidadEspecial = habilidadEspecial;
+        this.aliado = aliado;
+        this.enemigo = enemigo;
     }
 
     mostrarInformación() {
         return `ID: ${this.id}, 
-        Nombre: ${this.nombre},
-        Atributo: ${this.atributo},
-        Edad: ${this.edad}`;
+        Nombre ${this.nombreSuperhéroe},
+        NombreReal: ${this.nombreReal},
+        NombreSociedad: ${this.nombreSociedad},
+        Edad: ${this.edad},
+        PlanetaOrigen: ${this.planetaOrigen},
+        Debilidad: ${this.debilidad},
+        Poder: ${this.poder},
+        HabilidadEspecial: ${this.habilidadEspecial},
+        Aliado: ${this.aliado},
+        Enemigo: ${this.enemigo}`;
     }
 }
